c14603d843fd72fec01652b270e757811f1d91bb
Leland <leland@gplpoker.com>
2016-05-01 17:15:18 -0700

    Updated the resource controller render statements to be more modular

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -6,15 +6,27 @@ class ResourcesController < ApplicationController
                 else
                   Resource.all
                 end
-    # rubocop:disable Metrics/LineLength
-    render json: resources.to_json(include: ['addresses', 'phones', 'categories', 'notes', { services: { include: ['notes', { schedule: { include: 'schedule_days' } }] } }, { schedule: { include: 'schedule_days' } }])
-    # rubocop:enable Metrics/LineLength
+    render json: resources.to_json(RENDERED_RESOURCE_ATTRIBUTES)
   end
 
   def show
     resource = Resource.find(params[:id])
-    # rubocop:disable Metrics/LineLength
-    render json: resource.to_json(include: ['addresses', 'phones', 'categories', 'notes', { services: { include: ['notes', { schedule: { include: 'schedule_days' } }] } }, { schedule: { include: 'schedule_days' } }])
-    # rubocop:enable Metrics/LineLength
+    render json: resource.to_json(RENDERED_RESOURCE_ATTRIBUTES)
   end
+
+  RENDERED_SCHEDULE_ATTRIBUTES = {
+    include: 'schedule_days'
+  }.freeze
+
+  RENDERED_SERVICE_ATTRIBUTES = {
+    include: ['notes', { schedule: RENDERED_SCHEDULE_ATTRIBUTES }]
+  }.freeze
+
+  RENDERED_RESOURCE_ATTRIBUTES = {
+    include: ['addresses', 'phones', 'categories', 'notes', {
+      services: RENDERED_SERVICE_ATTRIBUTES
+    }, {
+      schedule: RENDERED_SCHEDULE_ATTRIBUTES
+    }]
+  }.freeze
 end

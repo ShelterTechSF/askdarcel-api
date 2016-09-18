98c8568e41c2fd1167566e85c6b8d4e70bbc37b7
Leland <leland@gplpoker.com>
2016-09-18 13:18:48 -0700

    [WIP] Add ratings/reviews API call #40

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :categories
   resources :resources do
+    resources :ratings
     collection do
       get :search
     end
+  end
+  resources :services do
+    resources :ratings
   end
 end

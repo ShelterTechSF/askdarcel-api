03ae1271b14ecf76b8e36aeaf89c253ea49e37d6
Benjamin Wilkins <ben.wilkins@airbnb.com>
2017-05-20 15:48:09 -0700

    Configure algolia index for Resource

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,8 @@
 class Resource < ActiveRecord::Base
+  include AlgoliaSearch
+
+  delegate :latitude, :longitude, to: :address, prefix: true, allow_nil: true
+
   enum status: { pending: 0, approved: 1, rejected: 2 }
 
   has_and_belongs_to_many :categories
@@ -16,5 +20,40 @@ class Resource < ActiveRecord::Base
 
   before_create do
     self.status = :pending unless status
+  end
+
+  algoliasearch do
+    geoloc :address_latitude, :address_longitude
+
+    add_attribute :address do
+      if address.present?
+        {
+          city: address.city,
+          state_province: address.state_province,
+          postal_code: address.postal_code,
+          country: address.country,
+        }
+      else
+        {}
+      end
+    end
+
+    add_attribute :notes do
+     notes.map {|n| n.note }
+    end
+
+    add_attribute :categories do
+      categories.map {|c| c.name }
+    end
+
+    add_attribute :keywords do
+     keywords.map {|k| k.name }
+    end
+
+    add_attribute :services do
+      services.map {|s| {
+        name: s.name,
+      }}
+    end
   end
 end

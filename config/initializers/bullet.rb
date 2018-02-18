932e2988ad2ef2b545de3c7869f2496589f54f63
Richard Xia <richardxia@richardxia.com>
2018-02-18 13:33:47 -0800

    Temporarily disable N+1 query detector.

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -7,6 +7,11 @@ unless Rails.env.production?
       Bullet.bullet_logger = true
       Bullet.raise = true
 
+      # TODO: We currently believe there's a bug in the N+1 query detector that
+      # causes it to detect a false positive w.r.t. the joins and prefetching
+      # between Resources and Categories as well as Services and Categories.
+      Bullet.n_plus_one_query_enable = false
+
       # With the upgrade to Rails 5, bullet has been raising false
       # positives. Disable for now.
       Bullet.unused_eager_loading_enable = false

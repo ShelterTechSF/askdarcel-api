74a81497b02909c5933ee4feb9ef2ffa32062bb2
Thomas Wolfe <thomas@grooveapp.com>
2018-07-01 12:53:23 -0700

    fix rubocop errors

--- app/models/service.rb
+++ app/models/service.rb
@@ -26,11 +26,10 @@ class Service < ActiveRecord::Base
     # Note: We can't use the per_environment option because both our production
     # and staging servers use the same RAILS_ENV.
     algoliasearch index_name: "#{Rails.configuration.x.algolia.index_prefix}_services_search", id: :algolia_id do # rubocop:disable Metrics/BlockLength,Metrics/LineLength
-      
       # specify the list of attributes available for faceting
       attributesForFaceting [:categories]
 
-      # Define attributes used to build an Algolia record 
+      # Define attributes used to build an Algolia record
       add_attribute :status
       add_attribute :_geoloc do
         if addresses.any?
@@ -108,8 +107,8 @@ class Service < ActiveRecord::Base
     id
   end
 
-  def is_mohcd_funded
-    categories.any?{|category| category['name'] === 'MOHCD Funded Services'}
+  def is_mohcd_funded # rubocop:disable Naming/PredicateName
+    categories.any? { |category| category['name'] == 'MOHCD Funded Services' }
   end
 
   def type

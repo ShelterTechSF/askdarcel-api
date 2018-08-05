12a69031416c97046a955222097d5a0c574d817f
Joe Freund <jfreund@myvest.com>
2018-08-05 14:28:44 -0700

    304 add certified timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180630205912) do
+ActiveRecord::Schema.define(version: 20180805190627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -277,6 +277,7 @@ ActiveRecord::Schema.define(version: 20180630205912) do
     t.integer  "contact_id"
     t.integer  "funding_id"
     t.string   "alternate_name"
+    t.datetime "certified_at"
     t.index ["contact_id"], name: "index_services_on_contact_id", using: :btree
     t.index ["funding_id"], name: "index_services_on_funding_id", using: :btree
     t.index ["program_id"], name: "index_services_on_program_id", using: :btree

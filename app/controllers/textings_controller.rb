4c3883aa38316e1a06beec5c4ddaea554428885d
fayceltouili <fayceltouili@gmail.com>
2020-12-16 00:39:21 -0800

    code improvements

--- app/controllers/textings_controller.rb
+++ app/controllers/textings_controller.rb
@@ -1,99 +1,116 @@
 class TextingsController < ApplicationController
 
   def create
+
+    recipient_name = texting_params[:recipient_name]
+    phone_number = parse_phone_number()
+    service_id = texting_params[:service_id]
+
     # Make a request to Textellent API. If request successful we update our DB
-    response = post_textellent(get_db_data)
+    data = get_db_data(recipient_name, phone_number, service_id)
+    response = post_textellent(data)
 
-    if response['status'] == 'success'
-      recipient = TextingRecipient.find_by(phone_number: texting_params[:phone_number])
-
-      if recipient
-        texting = Texting.new(
-          texting_recipient_id: recipient.id,
-          service_id: texting_params[:service_id]
-          )
-
-        if texting.save
-          render status: :ok, json: { message:'Message sent' }
-        else
-          render status: :bad_request
-        end
-
-      else
-        recipient = TextingRecipient.new(
-          recipient_name: texting_params[:recipient_name],
-          phone_number: texting_params[:phone_number]
-          )
-
-        recipient.save
-
-        texting = Texting.new(
-          texting_recipient_id: recipient.id,
-          service_id: texting_params[:service_id]
-          )
-
-        if texting.save
-          render status: :ok, json: { message:'Message sent' }
-        else
-          render status: :bad_request
-        end
-
-      end
-
-    else
-      render status: :bad_request, json: { error: response['message'] }
+    if response['status'] != 'success'
+      puts response['message']
+      render status: :bad_request, json: { error: 'Message not sent' }
+      return
     end
+
+    recipient = TextingRecipient.find_by(phone_number: phone_number)
+
+    unless recipient
+      recipient = create_new_recipient(recipient_name, phone_number)
+    else
+      update_recipient(recipient, recipient_name)
+    end
+
+    create_new_texting(recipient, service_id)
+    render status: :ok, json: { message:'Message sent' }
   end
 
   private
 
-  # Generate a data object to send to Textellent API
-  def get_db_data
-    recipient_name = texting_params[:recipient_name]
-    phone_number = texting_params[:phone_number]
-    service = Service.find(texting_params[:service_id])
-    category_ids = service.category_ids
-    resource = Resource.find(service.resource_id)
-    address = resource.addresses
-    phone = resource.phones[0].number
+  def create_new_recipient(recipient_name, phone_number)
+    TextingRecipient.create(
+      recipient_name: recipient_name,
+      phone_number: phone_number
+      )
+  end
 
-    categories = []
-    category_ids.each do |category_id|
-      categories << Category.find(category_id).name
-    end
+  def update_recipient(recipient, recipient_name)
+    recipient.update(recipient_name: recipient_name)
+  end
 
-    data = {
-    "firstName" => recipient_name,
-    "lastName" => "",
-    "mobilePhone" => phone_number,
-    "phoneAlternate": "",
-    "phoneHome" => "",
-    "phoneWork" => "",
-    "tags" => categories,
-    "engagementType" => "Resource Info",
-    "engagementInfo" => {
-        "Org_Name" => service.name,
-        "Org_Address1" => address[0].address_1,
-        "Org_Address2" => address[0].address_2,
-        "City" => address[0].city,
-        "State" => address[0].state_province,
-        "Zip" => address[0].postal_code,
-        "Org_Phone" => phone
-      }
-    }
-
-    data
+  def create_new_texting(recipient, service_id)
+    Texting.create(
+      texting_recipient_id: recipient.id,
+      service_id: service_id
+      )
   end
 
   def texting_params
     params.require(:data).permit(:recipient_name, :phone_number, :service_id)
   end
 
+  def parse_phone_number()
+    Phonelib.parse(texting_params[:phone_number], 'US').national(false)
+  end
+
+  # Generate a data object to send to Textellent API
+  def get_db_data(recipient_name, phone_number, service_id)
+
+    service = Service.includes(:categories).find(service_id)
+    categories = service.categories.map { |c| c.name }
+    resource = Resource.find(service.resource_id)
+    address = resource.addresses
+
+    phone = ''
+    if resource.phones.any?
+      phone = resource.phones[0].number
+    end
+
+    address_1 = ''
+    address_2 = ''
+    city = ''
+    state_province = ''
+    postal_code = ''
+
+    if address.any?
+      address_1 = address[0].address_1
+      address_2 = address[0].address_2 || ''
+      city = address[0].city
+      state_province =  address[0].state_province
+      postal_code = address[0].postal_code
+    end
+
+    data = {
+      "firstName" => recipient_name,
+      "lastName" => "",
+      "mobilePhone" => phone_number,
+      "phoneAlternate": "",
+      "phoneHome" => "",
+      "phoneWork" => "",
+      "tags" => categories,
+      "engagementType" => "Resource Info",
+      "engagementInfo" => {
+          "Org_Name" => service.name,
+          "Org_Address1" => address_1,
+          "Org_Address2" => address_2,
+          "City" => city,
+          "State" => state_province,
+          "Zip" => postal_code,
+          "Org_Phone" => phone
+        }
+    }
+
+    data
+  end
+
   # handling the post request to Textellent API.
   def post_textellent(data)
     header = {
       'Content-Type' => 'application/json',
-      'authCode' => ENV['TEXTELLENT_AUTH_CODE'],
+      'authCode' => Rails.configuration.x.textellent.api_key
     }
 
     query = {
--- app/models/texting_recipient.rb
+++ app/models/texting_recipient.rb
@@ -1,3 +1,4 @@
 class TextingRecipient < ApplicationRecord
   has_many :textings, dependent: :destroy
+  validates :phone_number, presence: true
 end

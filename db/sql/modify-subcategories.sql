f6691e7a74dd6bdc9987a037b492ed8a8ac0c738
Kelvin Ortiz <36288868+iopkelvin@users.noreply.github.com>
2021-01-03 14:05:40 -0800

    modified modify-subcategories.sql

--- db/sql/modify-subcategories.sql
+++ db/sql/modify-subcategories.sql
@@ -1,16 +1,17 @@
 -- "Rental Assistance and Eviction Prevention" Category
-insert into categories (id, created_at, updated_at, name, top_level, vocabulary, featured)
-values (1000004, '13-DEC-20', '13-DEC-20', 'Covid-housing', 'f', null, 'f');
+-- Already there
+-- insert into categories (id, created_at, updated_at, name, top_level, vocabulary, featured)
+-- values (1000004, '13-DEC-20', '13-DEC-20', 'Covid-housing', 'f', null, 'f');
 
 -- "Rental Assistance and Eviction Prevention" Subcategories
 insert into categories (id, created_at, updated_at, name, top_level, vocabulary, featured)
-values (1_400_001, '13-DEC-20', '13-DEC-20', 'My landlord gave me an eviction notice and I need legal help', 'f', null, 'f');
+values (1_400_001, '13-DEC-20', '03-JAN-21', 'My landlord gave me an eviction notice and I need legal help', 'f', null, 'f');
 insert into categories (id, created_at, updated_at, name, top_level, vocabulary, featured)
-values (1_400_002, '13-DEC-20', '13-DEC-20', 'My landlord told me I would get evicted and I need advice', 'f', null, 'f');
+values (1_400_002, '13-DEC-20', '03-JAN-21', 'My landlord told me I would get evicted and I need advice', 'f', null, 'f');
 insert into categories (id, created_at, updated_at, name, top_level, vocabulary, featured)
-values (1_400_003, '13-DEC-20', '13-DEC-20', 'I have not been able to pay my rent and I do not know what to do', 'f', null, 'f');
+values (1_400_003, '13-DEC-20', '03-JAN-21', 'I have not been able to pay my rent and I do not know what to do', 'f', null, 'f');
 insert into categories (id, created_at, updated_at, name, top_level, vocabulary, featured)
-values (1_400_004, '13-DEC-20', '13-DEC-20', 'I am not getting along with my neighbor(s) and /or my landlord and I need advice', 'f', null, 'f');
+values (1_400_004, '13-DEC-20', '03-JAN-21', 'I am not getting along with my neighbor(s) and /or my landlord and I need advice', 'f', null, 'f');
 
 -- Insert subcategories in "Rental Assistance and Eviction Prevention"
 insert into category_relationships (parent_id, child_id)

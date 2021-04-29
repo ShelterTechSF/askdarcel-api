44aa37b0aff89838c34f193e65d5410e834e980e
Katerina Kossler <katerinakossler@gmail.com>
2021-04-29 17:02:08 -0600

    fix: style changes for improved code clarity; status changes to better match put requests

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,16 +2,20 @@
 
 class AddressesController < ApplicationController
   def update
-    service = Service.find(params[:service_id])
-    address = Address.find(params[:id])
+    service = Service.find_by_id(params[:service_id])
+    address = Address.find_by_id(params[:id])
 
-    if !service.addresses.include?(address)
-      service.addresses << address
-      service.save!
-      render status: :created
-      return
+    if service && address
+      if service.addresses.include?(address)
+        render status: :ok
+        return
+      else
+        service.addresses << address
+        service.save!
+        render status: :created
+      end
     else
-      render status: :precondition_failed
+      render status: :bad_request
     end
   end
 end

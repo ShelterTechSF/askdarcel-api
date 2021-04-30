0605df79a43c8a7f6ab73a6d7ca948b8dda43be4
Katerina Kossler <katerinakossler@gmail.com>
2021-04-30 07:52:41 -0600

    fix: return route that was accidentally removed

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
   resources :services do
     resources :change_requests, only: :create
     resources :notes, only: :create
+    resources :feedbacks, only: %i[create index]
     resources :addresses, only: :update
     post :approve
     post :reject

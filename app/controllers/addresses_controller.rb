c7ba372e0280b3daef178b0ecaabc064d8b4228e
Katerina Kossler <katerinakossler@gmail.com>
2021-05-05 09:42:45 -0700

    fix: squash together private methods for improved readability; remove superfluous save call

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/MethodLength
 class AddressesController < ApplicationController
   def update
     service = Service.find_by_id(params[:service_id])
     address = Address.find_by_id(params[:id])
     if service && address
-      add_address_to_service(service, address)
-      return
+      if service.addresses.include?(address)
+        render status: :ok
+      else
+        service.addresses << address
+        service.save!
+        render status: :created
+      end
     else
       render status: :bad_request
     end
@@ -16,32 +22,15 @@ class AddressesController < ApplicationController
     service = Service.find_by_id(params[:service_id])
     address = Address.find_by_id(params[:id])
     if service && address
-      remove_address_from_service(service, address)
-      return
+      if service.addresses.include?(address)
+        service.addresses.delete(address)
+        render status: :no_content
+      else
+        render status: :ok
+      end
     else
       render status: :bad_request
     end
   end
-
-  private
-
-  def add_address_to_service(service, address)
-    if service.addresses.include?(address)
-      render status: :ok
-    else
-      service.addresses << address
-      service.save!
-      render status: :created
-    end
-  end
-
-  def remove_address_from_service(service, address)
-    if service.addresses.include?(address)
-      service.addresses.delete(address)
-      service.save!
-      render status: :no_content
-    else
-      render status: :ok
-    end
-  end
 end
+# rubocop:enable Metrics/MethodLength

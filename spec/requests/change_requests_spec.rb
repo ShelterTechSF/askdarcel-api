20bcfde50055c668dc2418dc99389369282067a9
Leland Garofalo <lelandg@gmail.com>
2021-08-11 20:15:15 -0700

    Updates to api doc generation and tests (#600)
    * Updates to api doc generation and tests

---
+++ spec/requests/change_requests_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'swagger_helper'
+
+RSpec.describe 'change_requests', type: :request do
+end
--- spec/requests/phones_spec.rb
+++ spec/requests/phones_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+require 'swagger_helper'
 
 RSpec.describe 'Phones' do
   describe '#destroy' do

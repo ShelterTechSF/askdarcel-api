7814b6cb1271624e648d0b30a1ae5c4d7d028dee
james7770 <jswift@myvest.com>
2022-09-27 09:27:19 -0700

    Trying to fix lint.  syntax is probably still wrong

--- app/controllers/news_article_controller.rb
+++ app/controllers/news_article_controller.rb
@@ -4,25 +4,22 @@ class NewsArticleController < ApplicationController
 
   def create
     news_article = params[:news_article]
-    persisted_inews_article = NewsArticle.create(news_article[:news_article] )
+    persisted_inews_article = NewsArticle.create(news_article[:news_article])
     render status: :created, json: NewsArticlePresenter.present(persisted_inews_article)
   end
 
   def retrieve
-    news_articles = NewsArticle.where("effective_date >= Time.current").and("(expiration_date is null or expiration_date > Time.current)")
+    news_articles = NewsArticle.where("effective_date is <= ?", Time.current)
+      .where("(expiration_date = ? or DATE(expiration_date) > ?)", nil, Time.current)")
     render json: NewsArticlePresenter.present(news_articles)
   end
 
   def update
     news_article = Instruction.find(params[:id])
-    news_article.headline = params[:news_article][:headline]
-    news_article.effective_date = params[:news_article][:effective_date]
-    news_article.body = params[:news_article][:body]
-    news_article.priority = params[:news_article][:priority]
-    news_article.expiration_date = params[:news_article][:expiration_date]
+    news_article = params[:news_article]
     news_article.save
 
-    render status: :ok, json: NewsArticlePresenter.present(headline)
+    render status: :ok, json: NewsArticlePresenter.present(news_article)
   end
 
   def delete

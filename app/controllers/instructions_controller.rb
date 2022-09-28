3be6d00f06ab2dc07270dae49213472713e8f3c5
Joe Freund <jfreund@myvest.com>
2022-09-27 22:04:55 -0700

    642 crud endpoints for instructions

--- app/controllers/instructions_controller.rb
+++ app/controllers/instructions_controller.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 class InstructionsController < ApplicationController
-
-  def index
-
-  	instructions = Instruction.where("id in (select id from instructions)")
-  	render json: InstructionsPresenter.present(instructions)
-
-  end
-
   def destroy
     instruction = Instruction.find params[:id]
     instruction.delete
@@ -17,20 +9,16 @@ class InstructionsController < ApplicationController
   end
 
   def create
-
-  	instruction = params[:instruction]
-    persisted_instruction = Instruction.create(service_id: instruction[:service_id], instruction: instruction[:instruction] )
-  	render status: :created, json: InstructionsPresenter.present(persisted_instruction)
+    instruction = params[:instruction]
+    persisted_instruction = Instruction.create(service_id: instruction[:service_id], instruction: instruction[:instruction])
+    render status: :created, json: InstructionsPresenter.present(persisted_instruction)
   end
 
   def update
-
     instruction = Instruction.find(params[:id])
     instruction.instruction = params[:instruction][:instruction]
     instruction.save
 
     render status: :ok, json: InstructionsPresenter.present(instruction)
-
   end
-
 end

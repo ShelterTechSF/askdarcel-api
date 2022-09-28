f5e579febbb29d23e50323c03cf1c2b4f7f76ef8
Joe Freund <jfreund@myvest.com>
2022-09-27 23:35:29 -0700

    649 crud endpoints for documents

--- app/presenters/documents_controller.rb
+++ app/presenters/documents_controller.rb
@@ -1,31 +1,28 @@
 # frozen_string_literal: true
 
 class DocumentsController < ApplicationController
+  def create
+    document = params[:document]
+    persisted_document = Document.create(url: document[:url], name: document[:name], description: document[:description])
 
-	def create
-    	document = params[:document]
-    	persisted_document = Document.create(url: document[:url], name: document[:name], description: document[:description])
+    service = Service.find_by_id(document[:service_id])
 
-    	service = Service.findById(document[:service_id])
+    service.documents << persisted_document
+    service.save
 
-    	persisted_document.services >> service
-    	persisted_document.save
+    render status: :created, json: DocumentPresenter.present(persisted_document)
+  end
 
+  def update
+    document = Document.find(params[:id])
+    document.url = params[:document][:url]
+    document.name = params[:document][:name]
+    document.description = params[:document][:description]
 
-    	render status: :created, json: DocumentPresenter.present(persisted_document)
-  	end
+    document.save
 
-
- 	def update
-    	document = Document.find(params[:id])
-    	document.url = params[:document][:url]
-    	document.name = params[:document][:name]
-    	document.description = params[:document][:description]
-    	
-    	document.save
-
-    	render status: :ok, json: DocumentPresenter.present(document)
-  	end
+    render status: :ok, json: DocumentPresenter.present(document)
+  end
 
   def destroy
     document = Document.find params[:id]
@@ -33,5 +30,4 @@ class DocumentsController < ApplicationController
 
     render status: :ok
   end
-
 end

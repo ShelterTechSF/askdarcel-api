1789f6d839f63362733873924231d275b4785e58
james7770 <jswift@myvest.com>
2022-10-04 16:02:24 -0700

    not sure the syntax but no lint errors

--- app/controllers/news_article_controller.rb
+++ app/controllers/news_article_controller.rb
@@ -16,12 +16,7 @@ class NewsArticleController < ApplicationController
 
   def update
     news_article = NewsArticle.find(params[:id])
-    news_article.headline = params[:news_article][:headline]
-    news_article.effective_date = params[:news_article][:effective_date]
-    news_article.body = params[:news_article][:body]
-    news_article.priority = params[:news_article][:priority]
-    news_article.expiration_date = params[:news_article][:expiration_date]
-
+    news_article << params[:news_article]
     news_article.save
 
     render status: :ok, json: NewsArticlePresenter.present(news_article)

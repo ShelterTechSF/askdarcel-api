7bdb6039d9dbfecb2a8defe3bd3f0e4758cc3099
james7770 <jswift@myvest.com>
2022-10-07 16:25:46 -0700

    disable length of method inspection

--- app/controllers/news_article_controller.rb
+++ app/controllers/news_article_controller.rb
@@ -14,14 +14,19 @@ class NewsArticleController < ApplicationController
 
     render json: NewsArticlePresenter.present(news_articles)
   end
-
+  # rubocop:disable Metrics/AbcSize
   def update
     news_article = NewsArticle.find(params[:id])
-    news_article << params[:news_article]
+    news_article.headline = params[:news_article][:headline]
+    news_article.effective_date = params[:news_article][:effective_date]
+    news_article.body = params[:news_article][:body]
+    news_article.priority = params[:news_article][:priority]
+    news_article.expiration_date = params[:news_article][:expiration_date]
     news_article.save
 
     render status: :ok, json: NewsArticlePresenter.present(news_article)
   end
+  # rubocop:enable Metrics/AbcSize
 
   def delete
     news_article = NewsArticle.find params[:id]

70c2225cca3bbd0412ddf2f1df12be0aeb3c7338
Brian Schroer <schroer.brianr@gmail.com>
2023-03-17 15:02:26 -0700

    Revert test commits

--- config/initializers/google_translate.rb
+++ config/initializers/google_translate.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
-# Rails.logger.info("6: #{Rails.configuration.x.google.translate_credentials}")
-puts("6: #{Rails.configuration.x.google.translate_credentials}")
-
-# if Rails.configuration.x.google.translate_credentials
-if Rails.env.production? || Rails.env.staging?
+if Rails.configuration.x.google.translate_credentials
   Rails.configuration.x.google.translation_enabled = true
   module CloudTranslation
     class << self; attr_accessor :project_id, :translation_service end
   end
   CloudTranslation.translation_service = Google::Cloud::Translate.translation_service
-  CloudTranslation.project_id = "askdarcel-184805"
+  CloudTranslation.project_id = JSON.parse(Rails.configuration.x.google.translate_credentials)['project_id']
 end

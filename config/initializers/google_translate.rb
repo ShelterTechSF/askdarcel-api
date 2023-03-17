456ad8246c6ceee569275af2a068b869dccf5fd8
Brian Schroer <schroer.brianr@gmail.com>
2023-03-17 13:17:02 -0700

    Google Translation Test

--- config/initializers/google_translate.rb
+++ config/initializers/google_translate.rb
@@ -3,11 +3,11 @@
 # Rails.logger.info("6: #{Rails.configuration.x.google.translate_credentials}")
 puts("6: #{Rails.configuration.x.google.translate_credentials}")
 
-if Rails.configuration.x.google.translate_credentials
-  Rails.configuration.x.google.translation_enabled = true
-  module CloudTranslation
-    class << self; attr_accessor :project_id, :translation_service end
-  end
-  CloudTranslation.translation_service = Google::Cloud::Translate.translation_service
-  CloudTranslation.project_id = JSON.parse(Rails.configuration.x.google.translate_credentials)['project_id']
+# if Rails.configuration.x.google.translate_credentials
+Rails.configuration.x.google.translation_enabled = true
+module CloudTranslation
+  class << self; attr_accessor :project_id, :translation_service end
 end
+CloudTranslation.translation_service = Google::Cloud::Translate.translation_service
+CloudTranslation.project_id = "askdarcel-184805"
+# end

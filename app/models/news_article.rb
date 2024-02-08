34430912924bc11b23271222e1f90a024d523dea
GeorgeCloud <net.george.sky@gmail.com>
2024-02-08 00:21:55 -0800

    add ability to query for active articles via NewsArticle model

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class NewsArticle < ActiveRecord::Base
+  scope :active, -> {
+    where("(effective_date is null or effective_date <= ?) and (expiration_date is null or expiration_date >= ?)",
+          Time.current, Time.current)
+  }
 end

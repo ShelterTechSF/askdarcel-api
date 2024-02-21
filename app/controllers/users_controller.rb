18d67bb2ce62f0d3dbda8c4b946adc20ecbffdf0
Brian Schroer <schroer.brianr@gmail.com>
2024-02-21 14:48:58 -0800

    689 Check user authentication token against auth0 prior to creating user. Ensure auth0 userId returned from validation hit matches user id sent from front-end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def create
-    user = User.create({ email: params[:email], name: params[:name], organization: params[:organization] })
-    render status: :created, json: UserPresenter.present(user)
-  end
+  before_action :authorize
 
-  def user_exists
-    user = User.find_by_email(params[:email])
-    render json: { user_exists: user ? true : false }
+  def create
+    # If userId param received via frontend (from Auth0) doesn't match @user_id from auth0 token validation,
+    # do not allow action
+    return render json: { error: 'Unauthorized' }, status: :unauthorized unless @user_id == params[:auth0_user_id]
+
+    user_params = params.permit(:email, :name, :organization)
+    user = User.new(user_params)
+
+    if user.save
+      render status: :created, json: UserPresenter.present(user)
+    else
+      render json: { error: 'Validation errors', messages: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotUnique
+    # User already exists. Don't do anything
   end
 end

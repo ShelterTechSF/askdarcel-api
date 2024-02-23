7328fc522f4bfde6f0f81e5787e5f58293b15b3b
Brian Schroer <schroer.brianr@gmail.com>
2024-02-23 13:59:36 -0800

    689 Add user_external_id column to user table. Small PR refactor

--- db/migrate/20231201213722_add_columns_to_user.rb
+++ db/migrate/20231201213722_add_columns_to_user.rb
@@ -1,6 +1,7 @@
 class AddColumnsToUser < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :organization, :string
+    add_column :users, :user_external_id, :string
     add_column :users, :email, :string
     add_index :users, :email, unique: true
   end

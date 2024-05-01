3d8f7d18540643b6bd84228eb9edfae4c407b3fe
Joe Freund <jfreund@myvest.com>
2024-04-30 18:01:46 -0700

    Make eligibilities hierarchical

--- db/migrate/20240501005404_make_eligibilities_hierarchical.rb
+++ db/migrate/20240501005404_make_eligibilities_hierarchical.rb
@@ -1,4 +1,4 @@
-class MakeCategoriesHierarchical < ActiveRecord::Migration[6.1]
+class MakeEligibilitiesHierarchical < ActiveRecord::Migration[6.1]
   def change
     add_column :eligibilities, :parent_id, :integer, null: true, index: true
     add_foreign_key :eligibilities, :eligibilities, column: :parent_id

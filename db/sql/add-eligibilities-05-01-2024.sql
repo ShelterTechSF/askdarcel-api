547e6014bd1cbdfa41248e61852c8a81927b2f5c
Leland Garofalo <lelandg@gmail.com>
2024-06-15 12:50:59 -0700

    chore(data): add eligibilities (#741)
    Co-authored-by: Leland Garofalo <lelandgarofalo@Lelands-MacBook-Pro.local>

---
+++ db/sql/add-eligibilities-05-01-2024.sql
@@ -0,0 +1,20 @@
+insert into eligibilities (name, created_at, updated_at)
+values ('Toddler Age',now(),now());
+
+insert into eligibilities (name, created_at, updated_at)
+values ('Preteen',now(),now());
+
+insert into eligibilities (name, created_at, updated_at)
+values ('Transitional Aged Youth (TAY)',now(),now());
+
+insert into eligibilities (name, created_at, updated_at)
+values ('Preschool Student',now(),now());
+
+insert into eligibilities (name, created_at, updated_at)
+values ('Elementary School Student',now(),now());
+
+insert into eligibilities (name, created_at, updated_at)
+values ('Postgraduate Student',now(),now());
+
+insert into eligibilities (name, created_at, updated_at)
+values ('Married without children',now(),now());
